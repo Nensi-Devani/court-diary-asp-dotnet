a5365b1b8298969bd069a9d0b82cdbb6d0d48aff
Krisha-Shira <krishashira698@gmail.com>
2026-03-14 17:48:36 +0530

    add meeting controller

---
+++ CourtDiary/Controllers/MeetingsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourtDiary.Controllers
+{
+    public class MeetingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
