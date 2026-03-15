4903377393b9a97d8136c256a8f8c364e7a28b6c
Krisha-Shira <krishashira698@gmail.com>
2026-03-15 08:48:01 +0530

    add case controller

---
+++ CourtDiary/Controllers/CasesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourtDiary.Controllers
+{
+    public class CasesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Viewdetails()
+        {
+            return View();
+        }
+    }
+}
