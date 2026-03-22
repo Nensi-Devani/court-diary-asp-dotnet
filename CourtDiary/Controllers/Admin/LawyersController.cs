e09d26715323eb5dff47b0bb3934b53f13bd975e
Krisha-Shira <krishashira698@gmail.com>
2026-03-22 18:29:21 +0530

    add lawyer controller

---
+++ CourtDiary/Controllers/Admin/LawyersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourtDiary.Controllers.Admin
+{
+    [Route("Admin/[controller]/[action]")]
+    public class LawyersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Admin/Lawyers/Index.cshtml");
+        }
+
+        public IActionResult Create()
+        {
+            return View("~/Views/Admin/Lawyers/Create.cshtml");
+        }
+
+        public IActionResult Edit()
+        {
+            return View("~/Views/Admin/Lawyers/Edit.cshtml");
+        }
+
+        public IActionResult Viewlawyer()
+        {
+            return View("~/Views/Admin/Lawyers/Viewlawyer.cshtml");
+        }
+    }
+}
