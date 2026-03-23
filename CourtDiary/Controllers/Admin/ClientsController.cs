4c53d7109998d2753e8b8061a76c450cf75a6726
Nensi Devani <ndevani894@rku.ac.in>
2026-03-23 10:54:21 +0530

    change names

---
+++ CourtDiary/Controllers/Admin/ClientsController.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourtDiary.Controllers.Admin
+{
+    [Route("Admin/[controller]/[action]")]
+    public class ClientsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Admin/Client/Index.cshtml");
+        }
+
+        public IActionResult Edit()
+        {
+            return View("~/Views/Admin/Client/Edit.cshtml");
+        }
+
+        public IActionResult Create()
+        {
+            return View("~/Views/Admin/Client/Create.cshtml");
+        }
+
+        public IActionResult Viewdetails()
+        {
+            return View("~/Views/Admin/Client/Viewdetails.cshtml");
+        }
+    }
+}
