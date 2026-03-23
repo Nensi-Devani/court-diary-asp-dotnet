0aec56200cfe595b996f52040363cd9944b73ca0
Nensi Devani <ndevani894@rku.ac.in>
2026-03-23 11:00:20 +0530

    renamed files and controller

---
+++ CourtDiary/Controllers/ClientsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourtDiary.Controllers
+{
+    public class ClientsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult ViewClient()
+        {
+            return View();
+        }
+    }
+}
