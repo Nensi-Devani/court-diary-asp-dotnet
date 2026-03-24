35dcee92b14c458637452c520ec970e38e6f205e
Nensi Devani <ndevani894@rku.ac.in>
2026-03-24 08:47:23 +0530

    add cases files

---
+++ CourtDiary/Controllers/Admin/CasesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourtDiary.Controllers.Admin
+{
+    [Route("Admin/[controller]/[action]")]
+    public class CasesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Admin/Cases/Index.cshtml");
+        }
+        public IActionResult Create()
+        {
+            return View("~/Views/Admin/Cases/Create.cshtml");
+        }
+
+        public IActionResult Edit()
+        {
+            return View("~/Views/Admin/Cases/Edit.cshtml");
+        }
+
+        public IActionResult Viewdetails()
+        {
+            return View("~/Views/Admin/Cases/Viewdetails.cshtml");
+        }
+    }
+}
