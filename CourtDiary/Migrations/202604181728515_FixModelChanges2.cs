1f982241a639ae381308ae8eba4811111bfc78f0
Krisha-Shira <krishashira698@gmail.com>
2026-04-21 11:53:56 +0530

    add migrations

---
+++ CourtDiary/Migrations/202604181728515_FixModelChanges2.cs
@@ -0,0 +1,24 @@
+﻿namespace CourtDiary.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FixModelChanges2 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("dbo.Cases", "user_id");
+            CreateIndex("dbo.Clients", "user_id");
+            AddForeignKey("dbo.Clients", "user_id", "dbo.Users", "user_id", cascadeDelete: true);
+            AddForeignKey("dbo.Cases", "user_id", "dbo.Users", "user_id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Cases", "user_id", "dbo.Users");
+            DropForeignKey("dbo.Clients", "user_id", "dbo.Users");
+            DropIndex("dbo.Clients", new[] { "user_id" });
+            DropIndex("dbo.Cases", new[] { "user_id" });
+        }
+    }
+}
---
+++ CourtDiary/Migrations/202604181946485_UpdateUserTable.cs
@@ -0,0 +1,16 @@
+﻿namespace CourtDiary.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateUserTable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
